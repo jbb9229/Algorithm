e1122e1962bf2c6e20c832fbffa53d17af8eedb8
jbb9229 <jbb9229@gmail.com>
2020-07-17 21:16:48 +0900

    add Algorithm Repository BracketConversion & StringCompression

---
+++ src/braket_conversion/Solution.java
@@ -0,0 +1,97 @@
+package braket_conversion;
+
+/**
+ * 2020 KAKAO BLIND RECRUITMENT
+ * 괄호 변환
+ * https://programmers.co.kr/learn/courses/30/lessons/60058
+ */
+
+class Solution {
+    public String solution(String p) {
+        String answer = "";
+        if (checkBrackets(p))
+            answer = p;
+        else{
+            answer = makePerfectBrackets(p);
+        }
+
+        return answer;
+    }
+
+    boolean checkBrackets(String s){
+        int sum = 0;
+        for (int i = 0; i < s.length(); i++) {
+            if(s.charAt(i) == '(')
+                sum++;
+            else if(s.charAt(i) == ')'){
+                if(sum <= 0){
+                    return false;
+                }else{
+                    sum--;
+                }
+            }
+        }
+
+        if(sum == 0)
+            return true;
+        else
+            return false;
+    }
+
+    String makePerfectBrackets(String s){
+        String u = detachBrackets(s)[0];
+        String v = detachBrackets(s)[1];
+        String answer = "";
+
+        if(checkBrackets(u)){
+            if(v.equals("")){
+                answer = u;
+            }else {
+                answer += u + makePerfectBrackets(v);
+            }
+        }else{
+            if(v.equals("")){
+                answer = "(" + ")" + changeBrackets(u);
+            }else
+                answer = "(" + makePerfectBrackets(v) + ")" + changeBrackets(u);
+        }
+
+        return answer;
+    }
+
+    String changeBrackets(String s){
+        String original = "";
+        String answer = "";
+
+        original = s.substring(1, s.length()-1);
+
+        for (int i = 0; i < original.length(); i++) {
+            if (original.charAt(i) == '(')
+                answer += ')';
+            else if (original.charAt(i) == ')')
+                answer += '(';
+        }
+
+        return answer;
+    }
+
+    String[] detachBrackets(String s){
+        String[] bracket = new String[2];
+
+        int leftBracket = 0;
+        int rightBracket = 0;
+        for (int i = 0; i < s.length(); i++) {
+            if(s.charAt(i) == '(')
+                leftBracket++;
+            if(s.charAt(i) == ')')
+                rightBracket++;
+            if(leftBracket == rightBracket){
+                bracket[0] = s.substring(0, i+1);
+                bracket[1] = s.substring(i+1, s.length());
+                break;
+            }
+        }
+
+        return bracket;
+    }
+}
---
+++ src/string_compression/Solution.java
@@ -0,0 +1,66 @@
+package string_compression;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+/**
+ * 2020 KAKAO BLIND RECRUITMENT
+ * 문자열 압축
+ * https://programmers.co.kr/learn/courses/30/lessons/60057
+ */
+
+class Solution {
+    public int solution(String s) {
+        List<String[]> cutList = new ArrayList<>();
+        ArrayList<Integer> cutSizes = new ArrayList<>();
+        cutSizes.add(s.length());
+
+        for (int i = 1; i <= s.length()/2; i++) {
+            String forSplit = "";
+            for (int j = 0; j < s.length(); j++) {
+                if((j % i) == 0 && j != 0){
+                    forSplit += "/";
+                }
+                forSplit += s.charAt(j);
+            }
+            //System.out.println(forSplit);
+            cutList.add(forSplit.split("/"));
+        }
+
+        for (int i = 0; i < cutList.size(); i++) {
+            int cutCount = 1;
+            int cutSize = 0;
+            String compression = "";
+            String prevCut = "";
+            String[] sar = cutList.get(i);
+            for (int j = 0; j < sar.length; j++) {
+                if (prevCut.equals(sar[j])){
+                    cutCount++;
+                }else{
+                    if (cutCount < 2)
+                        compression += prevCut;
+                    else
+                        compression += cutCount + prevCut;
+                    prevCut = sar[j];
+                    cutCount = 1;
+                }
+                if(j == sar.length - 1){
+                    if (cutCount < 2)
+                        compression += prevCut;
+                    else
+                        compression += cutCount + prevCut;
+                }
+            }
+            cutSize = compression.length();
+            cutSizes.add(cutSize);
+        }
+
+        Collections.sort(cutSizes);
+        // for(int size : cutSizes){
+        //     System.out.println(size);
+        // }
+
+        return cutSizes.get(0);
+    }
+}
