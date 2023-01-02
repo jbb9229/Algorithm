d47b094edcffe60883b3b71c6ccc07c999ab5263
jbb9229 <jbb9229@gmail.com>
2023-01-02 18:27:02 +0900

    문자열 뒤집기

---
+++ src/inflearn/string/reverseWord/Main.java
@@ -0,0 +1,31 @@
+package inflearn.string.reverseWord;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args){
+        Scanner in = new Scanner(System.in);
+        int wordCount = in.nextInt();
+        List<String> words = new ArrayList<>();
+        for (int i = 0; i < wordCount; i++) {
+            words.add(in.next());
+        }
+        Main m = new Main();
+        words.forEach(word -> System.out.println(m.reverseWord(word)));
+        return ;
+    }
+
+    private String reverseWord(String input) {
+        String answer = "";
+
+        for (int i=input.length()-1; i>=0; i--) {
+            answer += input.charAt(i);
+        }
+
+        return answer;
+    }
+
+}
